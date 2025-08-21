19c08d62f67d84711578a60c7eeee024381b633f
Kumar Vedant <kvedant1024@gmail.com>
2025-08-22 00:40:30 +0530

    Added modals, understood concept of modal and backdrop in react

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./header.css"; //Imported the header.css file in jsx file header.js, Although wherever we import css file, doesn't matter.
 
 function Header() {
+  const [showCart, setShowCart] = useState(false); //showCart is variable and setShowCart is function
+
+  function openCart() {
+    setShowCart(true);
+  }
+  function closeCart() {
+    setShowCart(false);
+  }
   return (
     <div className="header">
       <h1>My React Store</h1>
+
+      <div>
+        <button className="yellow-button" onClick={openCart}>
+          Cart
+        </button>
+      </div>
+      {showCart && ( //When setShowCart function returned true on click to Cart button, openCart function called setShowCart function which set the showCart variable as true and now we are into modal.
+        <div className="backdrop" onClick={closeCart}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            Hello from cart
+          </div>
+        </div>
+      )}
     </div>
   );
 }
